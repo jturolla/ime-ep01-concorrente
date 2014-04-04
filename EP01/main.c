f064aac952e4bfbf193647c1fe0d2164d6174664
Julio Turolla <julio.turolla@gmail.com>
2014-04-04 18:07:34 -0300

    Organizing files.

--- EP01/main.c
+++ EP01/main.c
@@ -5,48 +5,15 @@
 //  Created by Júlio Turolla Ribeiro on 02/04/14.
 //  Copyright (c) 2014 Julio Turolla. All rights reserved.
 //
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
 
-#include "data_types.h"
-#include "parse_input.c"
-
-void logPilot(char logMessage[], struct Pilot pilotData){
-    printf("[PILOTO %d | VOLTA %d | SPEED: %d | FUEL: %d] %s \n",
-           pilotData.pilotNumber, pilotData.currentLap, pilotData.currentSpeed, pilotData.fuelLevel, logMessage);
-}
-
-void *piloto(void *arg) {
-    int pilotNumber = (int) arg;
-
-    struct Pilot pilot;
-
-    pilot.pilotNumber = pilotNumber;
-    pilot.currentLap = 0;
-    pilot.fuelLevel = (totalLaps/2)+1;
-    pilot.currentSpeed = 0;
-
-    logPilot("PARTIU!", pilot);
-
-    while (pilot.currentLap != totalLaps) {
-
-        if (pilot.fuelLevel == 0) {
-            // Enter boxes
-            sleep(1); // Time to change the tires.
-            pilot.fuelLevel = (totalLaps/2)+1;
-        }
-
-        pilot.currentLap++;
-        pilot.fuelLevel--;
-        logPilot("COMPLETOU VOLTA!", pilot);
-    }
-
-    logPilot("PASSOU NA LINHA DE CHEGADA!", pilot);
-
-    return NULL;
-}
+#include "data.h"
+#include "helper.h"
+#include "pilot.h"
 
 int main(int argc, const char * argv[]) {
 
@@ -79,14 +46,14 @@ int main(int argc, const char * argv[]) {
     int myArray[10] = {
        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
     };
-
+    
     pthread_t threads[10];
 
     printf("Initiating Threads... \n");
 
     for (int i = 0; i < 10; i++) {
         int mychar = (int) myArray[i];
-        pthread_create(&threads[i], NULL, piloto, (void *)mychar);
+        pthread_create(&threads[i], NULL, piloto, (void*) mychar);
     }
 
     for (int i = 0; i < 10; i++) {
